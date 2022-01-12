87b3029715a7e8ab138ad403592fda12d2c4cb5a
IamSASD <sebastiansegdiaz@gmail.com>
2022-01-12 15:29:53 -0500

    show note in the page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import UI from './modules/UI.js';
+
 const buttonHead =  document.querySelector('.button-add-head');
 const buttonMain =  document.querySelector('.button-add-main');
 const popUp = document.querySelector('.pop-up-form');
@@ -9,7 +11,7 @@ function eventListeners(){
     buttonHead.addEventListener('click', showPopUp);
     buttonMain.addEventListener('click', showPopUp);
     exitPopUp.addEventListener('click', closedPopUp);
-    popUp.addEventListener('submit', getValues);
+    popUp.addEventListener('submit', manageValues);
 }
 
 function showPopUp(){
@@ -18,6 +20,26 @@ function showPopUp(){
 
 function closedPopUp(){
     wrapper.style.display = 'none';
+}
+
+let ui;
+
+function manageValues(e){
+
+    e.preventDefault();
+    const noteTitle = document.querySelector('.input-title').value;
+    const noteText = document.querySelector('.text-content').value;
+    const notesContainer = document.querySelector('.notes-container')
+
+    ui = new UI(noteTitle, noteText);
+
+    const card = ui.cardNote();
+    let divCreate = document.createElement('div');
+    card.innerHTML = card;
+
+    notesContainer.appendChild(divCreate);
+
+    closedPopUp();
 }
 
 
