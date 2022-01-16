b86536797ca6a2a984ecf879d39020c49cff7442
IamSASD <sebastiansegdiaz@gmail.com>
2022-01-16 14:32:07 -0500

    Hide the message to add a new note and change display flex to grid

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ const buttonMain =  document.querySelector('.button-add-main');
 const popUp = document.querySelector('.pop-up-form');
 const wrapper = document.querySelector('.wrapper');
 const exitPopUp = document.querySelector('.exit') ;
+const notesContainer = document.querySelector('.notes-container');
+const message = document.querySelector('.empty-container');
+
 
 eventListeners()
 function eventListeners(){
@@ -29,17 +32,31 @@ function manageValues(e){
     e.preventDefault();
     const noteTitle = document.querySelector('.input-title').value;
     const noteText = document.querySelector('.text-content').value;
-    const notesContainer = document.querySelector('.notes-container')
 
     ui = new UI(noteTitle, noteText);
 
     const card = ui.cardNote();
     let divCreate = document.createElement('div');
-    card.innerHTML = card;
+    divCreate.className = 'card-note';
+    divCreate.innerHTML = card;
 
     notesContainer.appendChild(divCreate);
 
+
     closedPopUp();
+    if(message.style.display != 'none'){
+        hideMessage();
+    }
 }
 
 
+function hideMessage(){
+    
+    
+    message.style.display = 'none';
+    notesContainer.classList.remove('notes-container');
+    notesContainer.classList.add('notes-container-grid');
+    console.log(notesContainer);
+
+    
+}
